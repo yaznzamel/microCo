46062839c9c7a4df626dd0d6bbe868c230b31a12
yazn zamel <yaznzamel2@gmail.com>
2025-03-13 21:10:03 +0300

    spring project initalizer

---
+++ src/main/java/com/yaznzamel/microco/MicrocoApplication.java
@@ -0,0 +1,13 @@
+package com.yaznzamel.microco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yaznzamel/microco/MicrocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaznzamel.microco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
